0f21821798bb97934666c570c0d9f69cae1afa10
Aditya Singh Rajput <aditharajput1023@gmail.com>
2025-05-23 21:14:05 +0530

    modified work

--- first.cpp
+++ first.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "I am Aditya Singh Rajput." << endl;
+    cout << "I am a C++ developer." << endl;     
     return 0;
 }
