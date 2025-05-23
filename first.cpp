12565f3a0602b771f5dab82c4152262785fd3ea5
Aditya Singh Rajput <aditharajput1023@gmail.com>
2025-05-23 21:21:56 +0530

    new branch

--- first.cpp
+++ first.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "I am Aditya Singh Rajput." << endl;
-    cout << "I am a C++ developer." << endl;     
+    cout << "I am a C++ developer." << endl;  
+    cout << "switch to new branch" << endl; 
     return 0;
 }
